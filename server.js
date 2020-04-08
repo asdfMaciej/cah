09ca331872ec54665f3a49b81be92a2b879fa8c4
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2020-04-08 17:24:05 +0200

    Storing players per nick basis & auto reconnecting & username caching
    At first, I tried to utilize disconnect event provided by sockets
    But it doesn't fire on broken connections [not handled gracefully]
    Built in ping system didn't seem to work for me, so I created my own
    Have to remove the attempted disconnect etc and add nick checking for new players
    It's quite crude and easy to abuse, but hey, it works

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ var socketIO = require('socket.io');
 var c = require('cardcast-api');
 
 var api = new c.CardcastAPI();
+/*
 api.deck('DADD6')
 	.then((results) => {
 		deck.black = results.calls.map(function(item) { 
@@ -40,15 +41,19 @@ api.deck('DADD6')
 		deck.black = deck.black.filter(c => {return strCount(c, '_') == 1;})
 		deck.white = results.responses.map((r) => {return r.text});
 		console.log(deck);
-		currentDeck.white = JSON.parse(JSON.stringify(shuffle(deck.white)));
-		currentDeck.black = JSON.parse(JSON.stringify(shuffle(deck.black)));
+		currentDeck.white = currentDeck.white.concat(shuffle(deck.white));
+		currentDeck.black = currentDeck.black.concat(shuffle(deck.black));
 		getRandomWhiteCards(1);
 		getRandomBlackCard();
 	});
-
+*/
 var app = express();
 var server = http.Server(app);
-var io = socketIO(server);
+
+var io = socketIO(server, {
+	pingInterval: 1000,
+	pingTimeout: 3000
+});
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -68,6 +73,13 @@ io.on('connection', function(socket) {
 });
 
 var players = {};
+var connections = {
+	// socket id -> nick
+};
+var pings = {
+	// socket id -> remaining
+}
+
 var deck = {
 	white: [
 		"Black Salami",
@@ -116,8 +128,8 @@ function getRandomBlackCard() {
 
 function sortedPlayers() {
 	let p = [];
-	for (let id in players) {
-		let player = players[id];
+	for (let nick in players) {
+		let player = players[nick];
 		p.push(player);
 	}
 	p.sort((a, b) => {return a.index - b.index});
@@ -134,7 +146,7 @@ function changeCzar() {
 		let player = p[n];
 		if (player.isCzar) {
 			nextCzar = parseInt(n)+1;
-			players[player.id].isCzar = false;
+			players[player.nick].isCzar = false;
 			break;
 		}
 	}
@@ -145,16 +157,16 @@ function changeCzar() {
 	nextCzar = p[nextCzar];
 	if (nextCzar == null) {
 		console.log('ERROR!!! no czar (quit?) - store czar index in round state');
-		players[p[0].id].isCzar = true;
+		players[p[0].nick].isCzar = true;
 		return;
 	}
 
-	players[nextCzar.id].isCzar = true;
+	players[nextCzar.nick].isCzar = true;
 }
 
 function getCzar() {
-	for (let id in players) {
-		let player = players[id];
+	for (let nick in players) {
+		let player = players[nick];
 		if (player.isCzar)
 			return player;
 	}
@@ -162,8 +174,8 @@ function getCzar() {
 }
 
 function checkIfAllPlayed() {
-	for (let id in players) {
-		let player = players[id];
+	for (let nick in players) {
+		let player = players[nick];
 		if (!player.isCzar && player.state == P_UNSELECTED)
 			return false;
 	}
@@ -171,11 +183,12 @@ function checkIfAllPlayed() {
 }
 
 const WIN_THRESHOLD = 8;
+const DISCONNECT_AFTER_SECONDS = 10;
 
 function checkForWin() {
-	for (let id in players)
-		if (players[id].points >= WIN_THRESHOLD)
-			return players[id];
+	for (let nick in players)
+		if (players[nick].points >= WIN_THRESHOLD)
+			return players[nick];
 	return false;
 }
 
@@ -193,17 +206,17 @@ function initRound() {
 initRound();
 
 function addNewCards() {
-	for (let id in players) {
-		if (!players[id].isCzar) {
+	for (let nick in players) {
+		if (!players[nick].isCzar) {
 			let cards = getRandomWhiteCards(round.blackFillPlaces);
-			players[id].cards = players[id].cards.concat(cards);
+			players[nick].cards = players[nick].cards.concat(cards);
 		}
 	}
 }
 
 function setPlayerState(state) {
-	for (let id in players) {
-		players[id].state = state;
+	for (let nick in players) {
+		players[nick].state = state;
 	}
 }
 
@@ -236,29 +249,53 @@ function nextRound() {
 
 function newRound() {
 	initRound();
-	let p = shuffle(sortedPlayers());
-	console.log(p);
-	for (let player of p)
+	for (let player of shuffle(sortedPlayers()))
 		initPlayer(player['id'], player['nick']);
+}
+
+function getPlayer(nick) {
+	return players[nick];
+}
+
+function getNick(socketID) {
+	return connections[socketID];
 }
 
 let playerIndex = 0;
 
+function initPlayer(socketID, nick) {
+	if (players[nick]) {
+		chat(`${nick} połączył się ponownie.`);
+		players[nick].triesLeft = -1;
 
+		disconnectPlayerSocket(nick);
 
-function initPlayer(id, nick) {
-	players[id] = {
+		connections[socketID] = nick;
+		pings[socketID] = DISCONNECT_AFTER_SECONDS;
+		players[nick].socketID = socketID;
+		return true;
+	}
+
+	players[nick] = {
 		nick: nick,
 		points: 0,
+		triesLeft: -1,
 		state: P_UNSELECTED,
 		index: playerIndex,
 		isCzar: Object.keys(players).length == 0,
 		cards: getRandomWhiteCards(8).concat(['_', '_']),
-		id: id
+		socketID: socketID
 	};
+
+	connections[socketID] = nick;
+	pings[socketID] = DISCONNECT_AFTER_SECONDS;
+
 	playerIndex += 1;
 	if (Object.keys(players).length === 1)
 		nextRound();
+
+	chat(`${nick} dołączył do gry.`);
+	return true;
 }
 
 function shufflePlayedCards() {
@@ -271,31 +308,61 @@ function shufflePlayedCards() {
 	console.log(c);
 }
 
+function disconnectPlayerSocket(nick) {
+	for (socketID in connections)
+		if (connections[socketID] == nick)
+			delete connections[socketID];
+
+	for (socketID in pings)
+		if (socketID == players[nick].socketID)
+			delete pings[socketID];
+}
+
+function disconnectPlayer(nick) {
+	if (players[nick].isCzar)
+		nextRound();
+
+	if (nick in round.whiteCards)
+		delete round.whiteCards[nick];
+
+	disconnectPlayerSocket(nick);
+
+	delete pings[players[nick].socketID];
+	delete players[nick];
+
+	
+
+	chat(`Rozłączył się ${nick}`);
+}
 
 const P_UNSELECTED = 0;
 const P_SELECTED = 1;
 const P_FINAL = 2;
-
+//
 io.on('connection', function(socket) {
+	var id = socket.id;
 	socket.on('JOIN', function(data) {
-		initPlayer(socket.id, data.nick);
-		console.log(`[*] Connect: ${socket.id} as ${data.nick}`);
-		chat(`Dołączył ${data.nick}`);
+		if(initPlayer(socket.id, data.nick)) {
+			console.log(`[*] Connect: ${socket.id} as ${data.nick}`);
+		}
 	});
 
 	socket.on('SELECT CARD', function(data) {
+		var nick = getNick(id);
+		var player = getPlayer(nick);
+
 		let card = data.card;
 		let cardIndex = data.index;
-		let id = socket.id;
-		if (players[id].isCzar || players[id].state != P_UNSELECTED)
-			return console.log(`[!] ${players[id].nick} sent card without perm!`);
 
-		if (!checkCardsMatch(players[id].cards[cardIndex], card))
-			return console.log(`[!] ${players[id].nick} card mismatch!`);
+		if (player.isCzar || player.state != P_UNSELECTED)
+			return console.log(`[!] ${player.nick} sent card without perm!`);
 
-		players[id].cards.splice(cardIndex, 1);
-		players[id].state = P_SELECTED;
-		round.whiteCards[id] = card;
+		if (!checkCardsMatch(player.cards[cardIndex], card))
+			return console.log(`[!] ${player.nick} card mismatch!`);
+
+		player.cards.splice(cardIndex, 1);
+		player.state = P_SELECTED;
+		round.whiteCards[nick] = card;
 
 		if(checkIfAllPlayed()) {
 			shufflePlayedCards();
@@ -304,49 +371,85 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('WIN CARD', function(data) {
-		let playerID = data.id;
-		let id = socket.id;
-		if (!players[id].isCzar || players[id].state != P_FINAL)
-			return console.log(`[!] ${players[id].nick} win card without perm!`);
+		var nick = getNick(id);
+		var player = getPlayer(nick);
+		let playerNick = data.nick;
 
-		console.log(round);
-		console.log(playerID);
+		if (!player.isCzar || player.state != P_FINAL)
+			return console.log(`[!] ${player.nick} win card without perm!`);
+
 		let czar = getCzar().nick;
-		let winning = round['whiteCards'][playerID];
+		let winning = round['whiteCards'][playerNick];
 		let black = round.blackCard.replace('_', winning);
-		players[playerID].points += 1;
+		players[playerNick].points += 1;
 
-		let msg = `${players[playerID].nick} wygrał turę (wybierał ${czar}):\n`;
+		let msg = `${players[playerNick].nick} wygrał turę (wybierał ${czar}):\n`;
 		msg += `${black}\n`;
 		chat(msg);
 		
 		nextRound();
 	});
 
+	socket.on('PING', function() {
+		socket.emit('PONG');
+		console.log('PING received');
+	});
+
+	socket.on('PONG', function() {
+		pings[socket.id] = DISCONNECT_AFTER_SECONDS;
+	});
+
 	socket.on('disconnect', function () {
-		let player = players[socket.id];
+		/*
+		var nick = getNick(id);
+		var player = getPlayer(nick);
 		if (!player)
 			return;
 
-		if (player.isCzar)
-			nextRound();
+		if (!nick)
+			return;
 
-		if (socket.id in round.whiteCards)
-			delete round.whiteCards[socket.id];
+		if (!players[nick])
+			return;
 
-		console.log(`[*] Disconnect: ${socket.id} as ${player.nick}`);
-		chat(`Rozłączył się ${player.nick}`);
-		delete players[socket.id];
+		players[nick].triesLeft = 10;
+		attemptDisconnect(nick);
+		
+		console.log(`[*] Disconnect: ${socket.id} as ${player.nick}`);*/
 	});
 });
+/*
+function attemptDisconnect(nick) {
+	let triesLeft = players[nick].triesLeft;
+	console.log(`Disconnect attempt for ${nick} - try: ${triesLeft}`);
+	if (triesLeft < 0)
+		return;
 
-
+	if (triesLeft === 0) {
+		disconnectPlayer(nick);
+	} else {
+		players[nick].triesLeft = triesLeft - 1;
+		setTimeout(attemptDisconnect, 1000, nick);
+	}
+}
+*/
 setInterval(function() {
 	io.sockets.emit('STATE', {
 		players: players,
 		round: round
 	});
 }, 500);
+
+setInterval(function() {
+	for (let socketID in pings) {
+		pings[socketID] -= 1;
+		if (pings[socketID] == 0) {
+			disconnectPlayer(getNick(socketID));
+			delete pings[socketID];
+		}
+	}
+	io.sockets.emit('PING', 1000);
+}, 1000);
 
 /*
 setInterval(function() {
--- static/game.js
+++ static/game.js
@@ -6,6 +6,31 @@ function shuffle(a) {
     return a;
 }
 
+function createCookie(name,value,hours) {
+	if (hours) {
+		var date = new Date();
+		date.setTime(date.getTime()+(hours*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
 var app = new Vue({
 	el: "#app",
 	data: {
@@ -13,15 +38,31 @@ var app = new Vue({
 		round: {},
 		chat: "",
 		socket: null,
-		id: null
+		id: null,
+		nick: null,
+		connected: false,
+		connectedTimeout: false,
 	},
 
 	mounted: function() {
-		this.socket = io();
+		this.socket = io(window.location.href, {'sync disconnect on unload':false});
+		let nick = readCookie('nick');
+		if (!nick) {
+			console.log('creating nick');
+			nick = prompt("Podaj jaki chcesz nick:", '');
+			createCookie('nick', nick, 4);
+		}
+
+		this.nick = nick;
+
+		this.socket.emit('JOIN', {nick: nick});
+
 		this.socket.on('connect', () => {
 			this.id = this.socket.io.engine.id;
+			this.connected = 3;
+			this.ping();
 		});
-		this.socket.emit('JOIN', {nick: prompt("Podaj jaki chcesz nick:", '')});
+		
 		this.socket.on('STATE', (state) => {
 			this.players = state.players;
 			this.round = state.round;
@@ -31,25 +72,52 @@ var app = new Vue({
 			let chat = document.querySelector("#chat");
 			chat.scrollTop = chat.scrollHeight;
 		});
+		this.socket.on('disconnect', () => {
+			this.connected = 1;
+		});
+
+		this.socket.on('PONG', () => {this.connected = 3;});
+		this.socket.on('PING', () => {this.socket.emit('PONG');});
 	},
 
 	methods: {
+		reconnect: function() {
+			console.log('connection lost - trying to reconnect');
+			if (this.connected) {
+				this.socket.emit('JOIN', {nick: this.nick});
+				console.log("connected - stopping attempts...");
+			} else {
+				this.socket.connect();
+				setTimeout(this.reconnect, 500);
+			}
+		},
+
+		ping: function() {
+			this.connected = this.connected - 1;
+			if (this.connected == 0) {
+				this.reconnect();
+			} else {
+				setTimeout(this.ping, 1000);
+				this.socket.emit('PING');
+			}
+		},
+
 		selectCard: function(card, index) {
 			if (card == "_")
 				card = prompt("Podaj zawartość karty:");
 			this.socket.emit('SELECT CARD', {card: card, index: index});
-			this.players[this.id].state = 1; // insta change
+			this.players[this.nick].state = 1; // insta change
 		},
 
-		winCard: function(playerID) {
-			this.socket.emit('WIN CARD', {id: playerID});
-		}
+		winCard: function(playerNick) {
+			this.socket.emit('WIN CARD', {nick: playerNick});
+		}//
 	},
 
 	computed: {
 		me: function() {
-			if (this.id in this.players)
-				return this.players[this.id];
+			if (this.nick in this.players)
+				return this.players[this.nick];
 			return {};
 		},
 
